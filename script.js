ca6a3c9c23bd6dade8b6789150764627f2fd7505
ppalladio <nine.slide@gmail.com>
2022-09-19 22:13:01 +0200

    map marker L.popup

--- script.js
+++ script.js
@@ -16,18 +16,35 @@ navigator?.geolocation.getCurrentPosition(
     (position) => {
         const { latitude, longitude } = position.coords;
 
-        const coords = [latitude, longitude]
-        const map = L.map('map').setView(coords, 20);
-        
+        const coords = [latitude, longitude];
+        const map = L.map('map').setView(coords, 15);
+
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
 
         L.marker(coords)
             .addTo(map)
             .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
             .openPopup();
+
+        map.on('click', (e) => {
+            const { lat, lng } = e.latlng;
+            L.marker({ lat, lng })
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup',
+                    }),
+                )
+                .setPopupContent('popup')
+                .openPopup();
+        });
     },
     () => {
         alert('Geolocation is not available');
