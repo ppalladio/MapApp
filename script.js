0c17258625b35eb2858101a8dbe4c149849378e9
ppalladio <nine.slide@gmail.com>
2022-09-19 21:23:39 +0200

    leaflet

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ navigator?.geolocation.getCurrentPosition(
     // get geolocation
     (position) => {
         const { latitude, longitude } = position.coords;
+
+        const coords = [latitude, longitude]
+        const map = L.map('map').setView(coords, 20);
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        L.marker(coords)
+            .addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     },
     () => {
         alert('Geolocation is not available');
